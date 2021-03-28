411a0031b6d5091ec1708b0f9ca634e42ecd4829
Amita <panwar.amita@gmail.com>
2021-03-27 23:06:32 -0400

    updated the connection string

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static("public"));
 //get the mongoose connection to the mongodb
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
 //set the routes 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
