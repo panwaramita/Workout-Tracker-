658c3ad7560fb3f217806dea491dc9abfb004949
Amita <panwar.amita@gmail.com>
2021-03-27 23:48:35 -0400

    updated the calcultation in the excercise

--- public/exercise.js
+++ public/exercise.js
@@ -113,8 +113,10 @@ async function handleFormSubmit(event) {
     workoutData.reps = Number(repsInput.value.trim());
     workoutData.duration = Number(resistanceDurationInput.value.trim());
   }
-
+  if(workoutData.name!="")
+  {
   await API.addExercise(workoutData);
+  }
   clearInputs();
   toast.classList.add("success");
 }
